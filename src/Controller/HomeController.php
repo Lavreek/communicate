d31065262f808c0259daad5139f3cb2757f6a03c
Lavreek <lavreeksolacki@yandex.ru>
2023-03-11 22:36:25 +0300

    Update: User auth

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_route_root')]
+    public function root(): Response
+    {
+        return $this->redirectToRoute('app_route_home');
+    }
+
+    #[Route('/home', name: 'app_route_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
